b31fee0e7eae0c98d816c7e18839385ecfa9d0fd
MauroBraga <M@r202150016774>
2023-04-30 12:29:47 -0300

    Componente  Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Post } from "./Post"
 import  { Header} from "./components/Header"
+import styels from './App.module.css';
 
 import './global.css';
+import { SideBar } from "./components/SideBar";
+
 
 function App() {
 
@@ -9,13 +12,20 @@ function App() {
     <div>
       <Header/>
 
-      <Post
-            author="Mauro"
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus debitis dolorem, ipsa eos error eius molestiae perspiciatis accusantium alias doloremque repudiandae consectetur sapiente cupiditate a unde minima, sed quo placeat?"/>
+      <div className={styels.wrapper}>
+            <SideBar/>
+            <main>
+                <Post
+                author="Mauro"
+                content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus debitis dolorem, ipsa eos error eius molestiae perspiciatis accusantium alias doloremque repudiandae consectetur sapiente cupiditate a unde minima, sed quo placeat?"/>
     
-      <Post
-            author="Diego"
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus debitis dolorem, ipsa eos error eius molestiae perspiciatis accusantium alias doloremque repudiandae consectetur sapiente cupiditate a unde minima, sed quo placeat?"/>
+                <Post
+                    author="Diego"
+                    content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus debitis dolorem, ipsa eos error eius molestiae perspiciatis accusantium alias doloremque repudiandae consectetur sapiente cupiditate a unde minima, sed quo placeat?"/>
+            </main>
+      </div>
+
+      
     </div>
    
   )
