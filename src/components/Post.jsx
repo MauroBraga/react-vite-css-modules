542332db6bc32b47b49865f296dfb7fd8f5bf568
MauroBraga <M@r202150016774>
2023-04-30 21:34:53 -0300

    estilização  do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,7 +6,7 @@ export function Post(){
             <header>
                 <div className={styles.author}>
                     <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/7256202?v=4"   />
-                    <div className={styles.autorInfo}>
+                    <div className={styles.authorInfo}>
                         <strong>Mauro Braga</strong>
                         <span>Web Developer</span>
                     </div>
@@ -18,9 +18,13 @@ export function Post(){
             <div className={styles.content}>
                     <p>Fala galeraa 👋</p>
                     <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                     <p><a href='#'>👉 jane.design/doctorcare</a></p>
-                     <p> <a href='#'>#novoprojeto #nlw #rocketseat 
-                     </a></p>
+                    <p><a href="">jane.design/doctorcare</a></p>
+                    <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                    <p>
+                        <a href="">#novoprojeto</a>{' '}
+                        <a href="">#nlw</a>{' '}
+                        <a href="">#rocketseat</a>
+                    </p>
             </div>
        </article>
     )
