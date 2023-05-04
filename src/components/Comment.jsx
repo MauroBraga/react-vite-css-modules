fd7b83c819a97900ab1828cffafc3a45e5e81a81
MauroBraga <M@r202150016774>
2023-05-03 21:03:18 -0300

    likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment({content,onDeleteComment}){
 
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content);
-      }
+    }
+
+    function handleLikeComent(){
+        setLikeCount(likeCount+1)
+    }
 
     return (
         <div className={styles.comment}>
@@ -25,9 +32,9 @@ export function Comment({content,onDeleteComment}){
                     <p>{content}</p>
                 </div>
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComent}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
